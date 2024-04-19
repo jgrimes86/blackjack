c38f78beedc0b6e84a5748b48b4b338ffe0d1367
Jim Grimes <jimgrimes86@gmail.com>
2024-04-19 15:40:28 -0400

    Created function to sort cards for totalling values; added calls to reset states

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
-import { Box, Button, ButtonGroup, Image, Stack, Text } from '@chakra-ui/react';
+import { Button, ButtonGroup, Image, Stack, Text } from '@chakra-ui/react';
+
+// Any cards with a value 'ACE' should come last in the card list
+function sortCardsByValue(card1, card2) {
+    if (card1.value === 'ACE') {
+        return 1
+    } else if (card2.value === 'ACE') {
+        return 1
+    } else {
+        return -1
+    }
+}
 
 function totalHandValue(cards) {
     if (cards) {
+        let sortedCards = cards.toSorted(sortCardsByValue)
         let values = 0
-        for (const card of cards) {
+        for (const card of sortedCards) {
             if (["JACK", "QUEEN", "KING"].includes(card.value)) {
                 values = values + 10
             } else if (card.value === "ACE") {
@@ -21,7 +33,6 @@ function totalHandValue(cards) {
     } else {
         return 0
     }
-
 }
 
 function Table() {
@@ -33,18 +44,17 @@ function Table() {
     const playerTotal = totalHandValue(playerCards)
     const dealerTotal = totalHandValue(dealerCards)
 
-    console.log('player turn: ',playerTurn)
-    console.log('player cards: ',playerCards)
-    console.log('dealer cards: ',dealerCards)
-    console.log('player total: ',playerTotal)
-    console.log('dealer total: ',dealerTotal)
-
+    // get cards from Deck of Cards API
     const dealCard = async (draw) => {
         return fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=${draw}`)
         .then(resp => resp.json())
     };
 
+    // start a new hand by drawing two cards for the player
     const handleStartGame = async () => {
+        setPlayerCards(() => [])
+        setPlayerTurn(true)
+        setDealerCards(() => [])
         setNewDeal(true)
         const firstCard = await dealCard(1)
         if (firstCard.success) {
@@ -56,6 +66,7 @@ function Table() {
         }
     };
 
+    // deal player one more card
     const handleHit = async () => {
         const newCard = await dealCard(1)
         if (newCard.success) {
@@ -63,18 +74,20 @@ function Table() {
         }
     }
 
+    // end player turn
     const handleStand = () => {
         setPlayerTurn(false)
     }
-
-    const dealerHand = dealerCards ? dealerCards.map((card, index) => {
-        return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
-    }) : null;
 
     const playerHand = playerCards ? playerCards.map((card, index) => {
         return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
     }) : null;
 
+    const dealerHand = dealerCards ? dealerCards.map((card, index) => {
+        return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
+    }) : null;
+
+    // buttons shown for player interaction
     const playerButtons = !newDeal
         ? <Button onClick={handleStartGame}>Place Bet</Button>
         : <ButtonGroup>
@@ -82,6 +95,7 @@ function Table() {
             <Button onClick={handleStand}>Stand</Button>
         </ButtonGroup>
     
+    // useEffect runs when page loads to retreive new deck info
     useEffect(() => {
         fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4')
         .then(resp => {
@@ -92,17 +106,52 @@ function Table() {
         })
     }, [])
 
+    // useEffect runs when player gets new card
     useEffect(() => {
-        if (!playerTurn) {
-            async function dealerTurn() {
+        if (playerHand && playerTotal) {
+            if (playerTotal === 21) {
+                console.log("PLAYER WINS!")
+                setNewDeal(false)
+            } else if (playerTotal > 21) {
+                console.log("PLAYER BUSTS!");
+                setNewDeal(false)
+            }
+        }
+    }, [playerHand])
+
+    // useEffect runs when player ends turn and when dealer draws cards
+    useEffect(() => {
+        if (!playerTurn && !dealerCards) {
+            // if dealer has no cards yet, get two cards
+            async function dealerFirstTurn() {
                 const cards = await dealCard(2)
                 if (cards.success) {
                     setDealerCards(cards.cards)
                 }
             }
-            dealerTurn()
+            dealerFirstTurn()
+        } else if (!playerTurn && dealerCards) {
+            // if dealer has cards, draw new card if dealer total less than 17
+            if (dealerTotal < 17) {
+                async function dealerDraw() {
+                    const card = await dealCard(1)
+                    if (card.success) {
+                        setDealerCards(currCards => [...currCards, card.cards[0]])
+                    }
+                }
+                dealerDraw()
+            } else {
+                if (dealerTotal > playerTotal && dealerTotal <= 21) {
+                    console.log("Dealer Wins")
+                } else if (dealerTotal === playerTotal) {
+                    console.log("Draw")
+                } else {
+                    console.log("PLAYER WINS!")
+                }
+                setNewDeal(false)
+            }
         }
-    }, [playerTurn])
+    }, [playerTurn, dealerCards])
 
     return (
         <div>
@@ -110,11 +159,13 @@ function Table() {
             <Stack direction='row' name='player hand'>
                 {dealerHand}
             </Stack>
+            <Text>Dealer card value: {dealerTotal}</Text>
             <Text>Player Hand</Text>
             <Stack direction='row' name='player hand'>
                 {playerHand}
             </Stack>
             {playerButtons}
+            <Text>Player card value: {playerTotal}</Text>
         </div>
     )
 }
