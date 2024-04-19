19eee8374aa273a669c8c2677f3b023c8621fb19
Jim Grimes <jimgrimes86@gmail.com>
2024-04-18 20:55:27 -0400

    Fetch and deal initial blackjack hand

---
+++ README.md
@@ -0,0 +1,5 @@
+
+[Deck of Cards API](https://www.deckofcardsapi.com/)
+
+[Blackjack rules](https://bicyclecards.com/how-to-play/blackjack/)
+
--- src/components/table.js
+++ src/components/table.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Box, Button, ButtonGroup, Image, Stack } from '@chakra-ui/react';
 
 function Table() {
     const [deck, setDeck] = useState({deck_id: "", remaining: 0})
-
-    console.log(deck)
+    const [hand, setHand] = useState([])
 
     useEffect(() => {
-        fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4')
         .then(resp => {
             if (resp.ok) {
                 resp.json()
@@ -15,8 +15,31 @@ function Table() {
         })
     }, [])
 
+    const handleDeal = () => {
+        fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=2`)
+        .then(resp => resp.json())
+        .then(deal => {
+            dealCard(deal.cards[0]);
+            setTimeout(dealCard, 500, deal.cards[1])
+        })
+    };
+
+    const dealCard = (card) => {
+        setHand(currentHand => [...currentHand, card])
+    };
+
+    console.log(hand)
+    const playerHand = hand ? hand.map((card, index) => {
+        return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
+    }) : null;
+
     return (
-        <div>The Table</div>
+        <div>
+            <Button onClick={handleDeal}>Place Bet</Button>
+            <Stack direction='row' name='player hand'>
+                {playerHand}
+            </Stack>
+        </div>
     )
 }
 
