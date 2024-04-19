b104835571c86547128c3b328e2d7a447b839bac
Jim Grimes <jimgrimes86@gmail.com>
2024-04-19 14:16:34 -0400

    Render dealer cards above player cards; added function to calculate player and dealer hand total values

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,43 @@
 import { useEffect, useState } from "react";
 import { Box, Button, ButtonGroup, Image, Stack, Text } from '@chakra-ui/react';
 
+function totalHandValue(cards) {
+    if (cards) {
+        let values = 0
+        for (const card of cards) {
+            if (["JACK", "QUEEN", "KING"].includes(card.value)) {
+                values = values + 10
+            } else if (card.value === "ACE") {
+                if (values <= 10) {
+                    values = values + 11
+                } else {
+                    values = values + 1
+                }
+            } else {
+                values = values + parseInt(card.value, 10)
+            }
+        }
+        return values
+    } else {
+        return 0
+    }
+
+}
+
 function Table() {
     const [deck, setDeck] = useState({deck_id: "", remaining: 0})
     const [newDeal, setNewDeal] = useState(false)
     const [playerCards, setPlayerCards] = useState([])
     const [playerTurn, setPlayerTurn] = useState(true)
     const [dealerCards, setDealerCards] = useState([])
+    const playerTotal = totalHandValue(playerCards)
+    const dealerTotal = totalHandValue(dealerCards)
 
     console.log('player turn: ',playerTurn)
     console.log('player cards: ',playerCards)
     console.log('dealer cards: ',dealerCards)
-
-    useEffect(() => {
-        fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4')
-        .then(resp => {
-            if (resp.ok) {
-                resp.json()
-                .then(data => setDeck(data))
-            }
-        })
-    }, [])
-
-    useEffect(() => {
-        if (!playerTurn) {
-            async function dealerTurn() {
-                const cards = await dealCard(2)
-                if (cards.success) {
-                    setDealerCards([cards.cards])
-                }
-            }
-            dealerTurn()
-        }
-    }, [playerTurn])
+    console.log('player total: ',playerTotal)
+    console.log('dealer total: ',dealerTotal)
 
     const dealCard = async (draw) => {
         return fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=${draw}`)
@@ -62,6 +67,10 @@ function Table() {
         setPlayerTurn(false)
     }
 
+    const dealerHand = dealerCards ? dealerCards.map((card, index) => {
+        return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
+    }) : null;
+
     const playerHand = playerCards ? playerCards.map((card, index) => {
         return <Image key={index} src={card.image} alt={`${card.value} of ${card.suit}`} boxSize='200px'/>
     }) : null;
@@ -72,11 +81,35 @@ function Table() {
             <Button onClick={handleHit}>Hit</Button>
             <Button onClick={handleStand}>Stand</Button>
         </ButtonGroup>
-        
+    
+    useEffect(() => {
+        fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=4')
+        .then(resp => {
+            if (resp.ok) {
+                resp.json()
+                .then(data => setDeck(data))
+            }
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!playerTurn) {
+            async function dealerTurn() {
+                const cards = await dealCard(2)
+                if (cards.success) {
+                    setDealerCards(cards.cards)
+                }
+            }
+            dealerTurn()
+        }
+    }, [playerTurn])
 
     return (
         <div>
             <Text>Dealer Hand</Text>
+            <Stack direction='row' name='player hand'>
+                {dealerHand}
+            </Stack>
             <Text>Player Hand</Text>
             <Stack direction='row' name='player hand'>
                 {playerHand}
